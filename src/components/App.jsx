59358ce281ba77e4652835ee05db11bbc6b6a2bc
=Andrei <balasca.andrei@gmail.com>
2023-07-18 18:58:57 +0300

    created a +1 button

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,25 @@
+import { useState } from 'react';
+
 export const App = () => {
+  const [value, setValue] = useState(0);
   return (
-    <div
+    <div className='container'
       style={{
         height: '100vh',
         display: 'flex',
         justifyContent: 'center',
         alignItems: 'center',
         fontSize: 40,
-        color: '#010101'
+        color: '#010101',
       }}
     >
       React homework template
+      <div>
+        {value}
+        <button type="button" onClick={() => setValue(value + 1)}>
+          Increment value by 1
+        </button>
+      </div>
     </div>
   );
 };
